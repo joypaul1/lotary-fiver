800120af4c76e07656ee1b290b17a5b3cd853fde
joypaul1 <jy5102555@gmail.com>
2021-07-04 00:01:32 +0600

    bg

---
+++ app/Http/Controllers/Backend/LotteryBgController.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Http\Controllers\Backend;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+
+class LotteryBgController extends Controller
+{
+    public function index()
+    {
+        
+    }
+}
